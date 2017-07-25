f7838377f7859400e792820bf2a88fd1e671a00c
Bastien Robert <bastienrbt@protonmail.com>
2017-07-25 20:46:15 +0200

    Init servers vars - Add password to rails-admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -38,4 +38,14 @@ RailsAdmin.config do |config|
     # history_index
     # history_show
   end
+
+  RailsAdmin.config do |config|
+    config.authorize_with do
+      authenticate_or_request_with_http_basic('Site Message') do |username, password|
+        username == ENV["ADMIN_LOGIN"] && password == ENV["ADMIN_PASSWORD"]
+      end
+    end
+    config.main_app_name { ['TradFood', 'Admin'] }
+  end
+
 end
