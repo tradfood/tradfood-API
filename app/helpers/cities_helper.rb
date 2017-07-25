a08573167ee3afe993964ec77d72fdc7962bff98
Bastien Robert <bastienrbt@protonmail.com>
2017-07-25 19:38:58 +0200

    Add filter array method to CitiesHelpers

--- app/helpers/cities_helper.rb
+++ app/helpers/cities_helper.rb
@@ -1,2 +1,30 @@
 module CitiesHelper
+
+  def filter_array
+    filter_array = {
+      # Difficulty array
+      'difficulty' => [
+        ['Tout', 'all'],
+        ['Facile', 1],
+        ['Moyen', 2],
+        ['Difficile', 3]
+      ],
+      # Season array
+      'season' => [
+        ['Tout', 'all'],
+        ['Été', 'summer'],
+        ['Automne', 'automn'],
+        ['Hiver', 'winter'],
+        ['Printemps', 'spring']
+      ],
+      # Menu array
+      'menu' => [
+        ['Tout', 'all'],
+        ['Entrée', 'starter'],
+        ['Plat', 'main course'],
+        ['Dessert', 'dessert']
+      ]
+    }
+  end
+
 end
