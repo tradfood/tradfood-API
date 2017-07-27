98ef2143fb3b039be6ce5af70ab17dbdd6af8e2e
Bastien Robert <bastienrbt@protonmail.com>
2017-07-27 22:56:23 +0200

    Helper method to translate difficulty & season on recipe

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,31 @@
 module RecipesHelper
+  def difficulty?(difficulty)
+    case difficulty
+    when 1
+      'Facile'
+    when 2
+      'Moyen'
+    when 3
+      'Difficile'
+    else
+      'ND'
+    end
+  end
+
+  def season?(season)
+    case season
+    when 'all'
+      'Tout'
+    when 'summer'
+      'Été'
+    when 'automn'
+      'Automne'
+    when 'spring'
+      'Printemps'
+    when 'winter'
+      'Hiver'
+    else
+      'ND'
+    end
+  end
 end
