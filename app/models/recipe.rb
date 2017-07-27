9b186908959de76db2130b726660e306991a6e8c
Bastien Robert <bastienrbt@protonmail.com>
2017-07-27 22:55:12 +0200

    Rails_admin : enum on string fields -> recipe : meal_type, difficulty, season

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,17 @@ class Recipe < ApplicationRecord
   belongs_to :city
   mount_uploader :image, ImageUploader
   before_save :name_downcase!
+
+  def meal_type_enum
+    [['Entrée', 'starter'],['Plat', 'main course'],['Dessert', 'dessert']]
+  end
+
+  def difficulty_enum
+    [['Facile', 1],['Moyen', 2],['Difficile', 3]]
+  end
+
+  def season_enum
+    [['Hiver', 'winter'],['Printemps', 'srping'],['Été', 'summer'],['Automne', 'automn']]
+  end
+
 end
