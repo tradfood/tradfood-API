06bab8582fefc93a1da90b16782f7de42f7cc9c1
Bastien Robert <bastienrbt@protonmail.com>
2017-07-27 23:18:53 +0200

    Markdown helper to convert mk->HTML with Redcarpet Gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,26 @@
 module ApplicationHelper
   include SlugHelper
+
+  # Markdown method using Redcarpet Gem : convert Markdown to HTML
+  def markdown(text)
+    options = {
+      filter_html:     true,
+      hard_wrap:       true,
+      link_attributes: { rel: 'nofollow', target: "_blank" },
+      space_after_headers: true,
+      fenced_code_blocks: true
+    }
+
+    extensions = {
+      autolink:           true,
+      superscript:        true,
+      disable_indented_code_blocks: true
+    }
+
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+    markdown.render(text).html_safe
+  end
+  
 end
